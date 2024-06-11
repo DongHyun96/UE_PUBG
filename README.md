bc8d69434f26098240b4780f77d20acce56816fe
DongHyun96 <wadanka09@gmail.com>
2024-06-11 22:40:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UE_PUBG
+
+Developed with Unreal Engine 5
---
+++ Source/UE_PUBG/UE_PUBGGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_PUBGGameMode.h"
+#include "UE_PUBGCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUE_PUBGGameMode::AUE_PUBGGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
