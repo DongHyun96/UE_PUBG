bd018dcc61e2d126e6fb2345a7a1b2a272f44b10
shb03008 <sshb03008@gmail.com>
2024-06-23 19:01:51 +0900

    InputMapping

--- Source/UE_PUBG/UE_PUBGGameMode.cpp
+++ Source/UE_PUBG/UE_PUBGGameMode.cpp
@@ -7,7 +7,7 @@
 AUE_PUBGGameMode::AUE_PUBGGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Project_PUBG/Common/Character/BPC_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
