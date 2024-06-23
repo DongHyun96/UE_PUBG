ddf60518b4b1d86fa6719d2b628937c0e8d40476
DongHyun96 <wadanka09@gmail.com>
2024-06-23 18:59:39 +0900

    Anim캐릭터 클래스 Update 함수내용 추가

--- Source/UE_PUBG/Character/C_AnimBasicCharacter.cpp
+++ Source/UE_PUBG/Character/C_AnimBasicCharacter.cpp
@@ -3,10 +3,34 @@
 
 #include "Character/C_AnimBasicCharacter.h"
 
+#include "Character/C_BasicCharacter.h"
+
 void UC_AnimBasicCharacter::NativeBeginPlay()
 {
+	Super::NativeBeginPlay();
+
+	OwnerCharacter = Cast<AC_BasicCharacter>(TryGetPawnOwner());
 }
 
 void UC_AnimBasicCharacter::NativeUpdateAnimation(float DeltaSeconds)
 {
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!OwnerCharacter) return;
+
+	Speed = OwnerCharacter->GetVelocity().Size2D();
+
+	//bIsFalling = OwnerCharacter->GetCharacterMovement()->IsFalling();
+
+	//const FRotator Rotation = (OwnerCharacter->GetCurWeaponType() == EWeaponType::UNARMED) ?
+	//	OwnerCharacter->Controller->GetControlRotation() : OwnerCharacter->GetActorRotation();
+
+	const FRotator Rotation = OwnerCharacter->GetActorRotation();
+
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	Direction = CalculateDirection(OwnerCharacter->GetVelocity().GetSafeNormal2D(), YawRotation);
+
+	HandState = OwnerCharacter->GetHandState();
+	PoseState = OwnerCharacter->GetPoseState();
 }
