c4681f8ee83a13211de5fd9d73864c3c9ff364be
DongHyun96 <wadanka09@gmail.com>
2024-06-24 13:49:12 +0900

    Revert "Merge pull request #44 from DongHyun96/DongHyun_Branch"
    This reverts commit 8237e1d43cda8e34012d950ca94ad4d05abb9d59, reversing
    changes made to f8ad4047b0eb4e101be8d47c5f7986fb52edb3ae.

--- Source/UE_PUBG/UE_PUBGGameMode.cpp
+++ Source/UE_PUBG/UE_PUBGGameMode.cpp
@@ -1,4 +1,3 @@
-
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UE_PUBGGameMode.h"
@@ -7,8 +6,9 @@
 
 AUE_PUBGGameMode::AUE_PUBGGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Project_PUBG/Common/Character/BPC_Player"));
+
+// set default pawn class to our Blueprinted character
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
