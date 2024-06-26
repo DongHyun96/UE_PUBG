85dd515ca87562ef73dd067458595dad53d3ddb2
DongHyun96 <wadanka09@gmail.com>
2024-06-26 22:12:54 +0900

    Weapon 기본 클래스 구조 생성

---
+++ Source/UE_PUBG/Weapon/WeaponStrategy/C_ThrowingWeaponStrategy.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/WeaponStrategy/C_ThrowingWeaponStrategy.h"
+
+bool UC_ThrowingWeaponStrategy::UseBKeyStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseRKeyStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseMlb_StartedStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseMlb_OnGoingStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseMlb_CompletedStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseMrb_StartedStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseMrb_OnGoingStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
+
+bool UC_ThrowingWeaponStrategy::UseMrb_CompletedStrategy(C_BasicCharacter* WeaponUser, AC_Weapon* Weapon)
+{
+	return false;
+}
