50ee0990617bc42ee1bd7525fa93c0690965fdfe
DongHyun96 <wadanka09@gmail.com>
2024-06-28 23:25:53 +0900

    Main Character DH로 다시 수정

--- Source/UE_PUBG/UE_PUBGGameMode.cpp
+++ Source/UE_PUBG/UE_PUBGGameMode.cpp
@@ -8,7 +8,9 @@ AUE_PUBGGameMode::AUE_PUBGGameMode()
 {
 	//Change to Playable Character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Project_PUBG/Common/Character/BPC_Player"));
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Project_PUBG/Hyunho/Character/BPC_Player_HH"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Project_PUBG/Hyunho/Character/BPC_Player_HH"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Project_PUBG/DongHyun/Copy/DH_BPC_Player"));
+
 
 // set default pawn class to our Blueprinted character
 	if (PlayerPawnBPClass.Class != NULL)
