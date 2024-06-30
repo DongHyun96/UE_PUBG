05971d086674d70575d2f1228aac388f6d3f63c2
DongHyun96 <wadanka09@gmail.com>
2024-06-30 16:16:35 +0900

    Change attributes

--- Source/UE_PUBG/Character/C_Player.cpp
+++ Source/UE_PUBG/Character/C_Player.cpp
@@ -193,6 +193,8 @@ void AC_Player::HandleTurnInPlace() // Update함수 안에 있어서 좀 계속 호출이 되�
 	// 현재 멈춰있는 상황이 아니면 처리 x
 	if (GetVelocity().Size() > 0.f) return;
 
+	if (bIsHoldDirection) return;
+
 	float Delta = UKismetMathLibrary::NormalizedDeltaRotator(GetControlRotation(), GetActorRotation()).Yaw;
 
 	// Debugging
