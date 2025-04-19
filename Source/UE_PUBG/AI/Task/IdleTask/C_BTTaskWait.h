f8853f8e908f3b8b64cea6742b7210d04741ff60
DongHyun96 <wadanka09@gmail.com>
2025-04-19 14:36:55 +0900

    StateMachine Service 내용 WaitTask TickTask 제대로 처리되도록 바꾸면서 WaitTask 쓰는 것으로 수정

--- Source/UE_PUBG/AI/Task/IdleTask/C_BTTaskWait.h
+++ Source/UE_PUBG/AI/Task/IdleTask/C_BTTaskWait.h
@@ -10,7 +10,7 @@
  * 
  */
 UCLASS()
-class UE_PUBG_API UC_BTTaskWait : public UBTTask_Wait
+class UE_PUBG_API UC_BTTaskWait : public UBTTaskNode
 {
 	GENERATED_BODY()
 
@@ -21,4 +21,32 @@ public:
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	
+	/// <summary>
+	/// <para> WaitTime이 남아있을 때 처리될 내용 </para>
+	/// <para> 아직 WaitTime이 모두 끝나지 않았을 때의 State handling </para>
+	/// </summary>
+	void OnWaitTimeRemain(UBehaviorTreeComponent& OwnerComp, class AC_EnemyAIController* EnemyAIController, class AC_Enemy* Enemy, class UC_BehaviorComponent* EnemyBehaviorComponent);
+
+	/// <summary>
+	/// WaitTime이 모두 끝났을 때 처리될 내용
+	/// </summary>
+	void OnWaitTimeFinished(UBehaviorTreeComponent& OwnerComp, AC_EnemyAIController* EnemyAIController, AC_Enemy* Enemy, UC_BehaviorComponent* EnemyBehaviorComponent);
+
+private:
+	
+	/// <summary>
+	/// Random한 Target 지점 setting 후 이동 
+	/// </summary>
+	/// <param name="Enemy"></param>
+	/// <param name="EnemyBehaviorComponent"></param>
+	/// <param name="bHasToMoveInsideMainCircle"> : Random한 지점이 MainCircle 내부의 지점이어야 하는지 체크 </param>
+	void ExecuteMoveToRandomLocation(class AC_Enemy* Enemy, class UC_BehaviorComponent* EnemyBehaviorComponent, bool bHasToMoveInsideMainCircle);
+
+private:
+
+	TMap<class UC_BehaviorComponent*, float> EnemyTimers{};
+	
 };
