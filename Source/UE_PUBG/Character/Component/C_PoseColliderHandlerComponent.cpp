eccc8402f2f1640dee741faa6c1b1971eb91d803
DongHyun96 <wadanka09@gmail.com>
2025-06-04 18:43:07 +0900

    Trace Collision Channel 추가 및 적용

--- Source/UE_PUBG/Character/Component/C_PoseColliderHandlerComponent.cpp
+++ Source/UE_PUBG/Character/Component/C_PoseColliderHandlerComponent.cpp
@@ -10,6 +10,8 @@
 #include "HUD/C_InformWidget.h"
 #include "Utility/C_Util.h"
 
+const ECollisionChannel UC_PoseColliderHandlerComponent::PoseCheckerChannel = ECC_GameTraceChannel1;
+
 const TMap<EPoseState, TPair<float, float>> UC_PoseColliderHandlerComponent::POSE_BY_ROOTCOLLIDER_HEIGHT_RADIUS =
 {
 	{EPoseState::STAND,		{88.f, 34.f}},
@@ -114,7 +116,7 @@ bool UC_PoseColliderHandlerComponent::CanChangePoseOnCurrentSurroundEnvironment(
 			StartLocation,
 			DestLocation,
 			FQuat::Identity,
-			ECC_Visibility,
+			PoseCheckerChannel,
 			FCollisionShape::MakeSphere(SWEEP_SPHERE_RAD),
 			CollisionParams
 		);
@@ -149,7 +151,7 @@ bool UC_PoseColliderHandlerComponent::CanChangePoseOnCurrentSurroundEnvironment(
 			StartLocation,
 			DestLocation,
 			FQuat::Identity,
-			ECC_Visibility,
+			PoseCheckerChannel,
 			FCollisionShape::MakeSphere(SWEEP_SPHERE_RAD),
 			CollisionParams
 		);
@@ -276,8 +278,8 @@ float UC_PoseColliderHandlerComponent::GetCrawlSlopeAngle
 	FHitResult HeadHitResult{};
 	FHitResult PelvisHitResult{};
 
-	bool HasHeadHit		= GetWorld()->LineTraceSingleByChannel(HeadHitResult, HeadStartLocation, HeadDestLocation, ECC_Visibility, CollisionParams);
-	bool HasPelvisHit	= GetWorld()->LineTraceSingleByChannel(PelvisHitResult, PelvisStartLocation, PelvisDestLocation, ECC_Visibility, CollisionParams);
+	bool HasHeadHit		= GetWorld()->LineTraceSingleByChannel(HeadHitResult, HeadStartLocation, HeadDestLocation, PoseCheckerChannel, CollisionParams);
+	bool HasPelvisHit	= GetWorld()->LineTraceSingleByChannel(PelvisHitResult, PelvisStartLocation, PelvisDestLocation, PoseCheckerChannel, CollisionParams);
 
 	if (EnableDebugLine)
 	{
@@ -340,8 +342,8 @@ float UC_PoseColliderHandlerComponent::GetCrawlSlopeDegree
 	//FHitResult HeadHitResult{};
 	//FHitResult PelvisHitResult{};
 
-	//bool HasHeadHit		= GetWorld()->LineTraceSingleByChannel(HeadHitResult, HeadStartLocation, HeadDestLocation, ECC_Visibility, CollisionParams);
-	//bool HasPelvisHit	= GetWorld()->LineTraceSingleByChannel(PelvisHitResult, PelvisStartLocation, PelvisDestLocation, ECC_Visibility, CollisionParams);
+	//bool HasHeadHit		= GetWorld()->LineTraceSingleByChannel(HeadHitResult, HeadStartLocation, HeadDestLocation, PoseCheckerChannel, CollisionParams);
+	//bool HasPelvisHit	= GetWorld()->LineTraceSingleByChannel(PelvisHitResult, PelvisStartLocation, PelvisDestLocation, PoseCheckerChannel, CollisionParams);
 
 	//if (EnableDebugLine)
 	//{
