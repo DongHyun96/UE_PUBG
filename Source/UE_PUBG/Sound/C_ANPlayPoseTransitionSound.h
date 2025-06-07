f3008cd60fd70f18c86c4db17f438d0fcc060eb3
DongHyun96 <wadanka09@gmail.com>
2025-06-07 21:19:53 +0900

    etc.

--- Source/UE_PUBG/Sound/C_ANPlayPoseTransitionSound.h
+++ Source/UE_PUBG/Sound/C_ANPlayPoseTransitionSound.h
@@ -7,7 +7,7 @@
 #include "C_ANPlayPoseTransitionSound.generated.h"
 
 /**
- * 이 클래스를 기반으로 하는 6개의 PoseTransition AN 블루프린트 생성해서 해당 sound를 Transition에 따라 초기화 시켜줄 예정
+ * 이 클래스를 기반으로 하는 4개의 PoseTransition AN 블루프린트 생성해서 해당 sound를 Transition에 따라 초기화 시켜줄 예정
  */
 UCLASS()
 class UE_PUBG_API UC_ANPlayPoseTransitionSound : public UAnimNotify
