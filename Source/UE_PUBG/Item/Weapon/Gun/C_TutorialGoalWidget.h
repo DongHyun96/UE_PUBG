a3639bc81a93a424c4e020975978b39f8625afec
DongHyun96 <wadanka09@gmail.com>
2025-06-29 17:05:32 +0900

    Tutorial Goal side UI 작업 중

---
+++ Source/UE_PUBG/Item/Weapon/Gun/C_TutorialGoalWidget.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "C_TutorialGoalWidget.generated.h"
+
+UCLASS()
+class UE_PUBG_API UC_TutorialGoalWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
