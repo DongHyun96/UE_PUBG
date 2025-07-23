56861abeefd3cadbaa94ff99d87b97d3fa774835
DongHyun96 <wadanka09@gmail.com>
2025-07-23 17:10:55 +0900

    이동식 표적지 구현

---
+++ Source/UE_PUBG/TrainingLevel/TrainingShootingTarget/C_MovableShootingTarget.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_MovableShootingTarget.h"
+
+
+AC_MovableShootingTarget::AC_MovableShootingTarget()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* RootComp = CreateDefaultSubobject<USceneComponent>(FName("Root"));
+	SetRootComponent(RootComp);
+
+	Anchor0 = CreateDefaultSubobject<USceneComponent>(TEXT("AnchorOne"));
+	Anchor1 = CreateDefaultSubobject<USceneComponent>(TEXT("AnchorTwo"));
+	Anchor0->SetupAttachment(RootComponent);
+	Anchor1->SetupAttachment(RootComponent);
+}
+
+void AC_MovableShootingTarget::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LeftLocation  = Anchor0->GetComponentLocation();
+	RightLocation = Anchor1->GetComponentLocation();
+}
+
+void AC_MovableShootingTarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	ElapsedTime += DeltaTime;
+	
+	float Alpha = (FMath::Sin(ElapsedTime * MoveSpeed) + 1.0f) * 0.5f;
+
+	FVector NewLocation = FMath::Lerp(LeftLocation, RightLocation, Alpha);
+	this->SetActorLocation(NewLocation);
+}
