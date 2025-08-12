74226ccdb2adb5498fd6a3a7497d4d80b95e557a
DongHyun96 <wadanka09@gmail.com>
2025-08-12 10:37:22 +0900

    CombatTester용 MoveTo 추가 기능 추가
    AttackPoint 이동 중 TargetCharacter가 시야에 잡혔다면, 다음 공격 Sequence 진행

---
+++ Source/UE_PUBG/AI/Task/TesterTask/C_BTTaskCombatTestMoveTo.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_BTTaskCombatTestMoveTo.h"
+
+#include "AI/C_BehaviorComponent.h"
+#include "AI/C_EnemyAIController.h"
+#include "Character/C_BasicCharacter.h"
+
+UC_BTTaskCombatTestMoveTo::UC_BTTaskCombatTestMoveTo()
+{
+}
+
+void UC_BTTaskCombatTestMoveTo::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+
+	AC_EnemyAIController* Controller = Cast<AC_EnemyAIController>(OwnerComp.GetAIOwner());
+
+	AC_BasicCharacter* TargetCharacter = Controller->GetBehaviorComponent()->GetTargetCharacter();
+
+	// 공격 지점 이동 중, Combat Test TargetCharacter가 사망 시, Combat sequence 종료
+	if (!TargetCharacter || TargetCharacter->GetMainState() == EMainState::DEAD)
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+
+	// 공격 지점 이동 중, TargetCharacter가 시야에 잡힌 상황
+	if (Controller->IsCurrentlyOnSight(TargetCharacter))
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+}
+
+EBTNodeResult::Type UC_BTTaskCombatTestMoveTo::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/UE_PUBG/AI/Task/TesterTask/C_BTTaskCombatTestMoveTo.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/Task/IdleTask/C_BTTaskMoveTo.h"
+#include "C_BTTaskCombatTestMoveTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_PUBG_API UC_BTTaskCombatTestMoveTo : public UC_BTTaskMoveTo
+{
+	GENERATED_BODY()
+
+public:
+
+	UC_BTTaskCombatTestMoveTo();
+
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+};
